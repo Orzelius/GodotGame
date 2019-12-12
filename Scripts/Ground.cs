62e61ac721127891f2909abc6d9f83e07cb29c8c
Albert Kostusev <albertkostusev@gmail.com>
2019-12-12 09:52:13 +0200

    Work on world gen

--- Scripts/Ground.cs
+++ Scripts/Ground.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 public class Ground : Node2D
 {
@@ -16,43 +17,56 @@ public class Ground : Node2D
     [Export]
     public float maxLengthY = 100f;
     [Export]
-    public float totalLenght = 600;
+    public float totalLenght = 2000f;
     [Export]
     Color color = new Color(51, 51, 240);
     [Export]
     public float width = 5;
+    [Export]
+    public float sizeMod = 1;
     // Called when the node enters the scene tree for the first time.
+    private RandomNumberGenerator rnd = new RandomNumberGenerator();
     public override void _Draw()
     {
-        var rnd = new RandomNumberGenerator();
+        maxLengthX = maxLengthX/sizeMod;
+        maxLengthY = maxLengthY/sizeMod;
+        minLengthX = minLengthX/sizeMod;
+        minLengthY = minLengthY/sizeMod;
+        
         rnd.Randomize();
         Vector2 screenSize = GetViewport().GetVisibleRect().Size;
         List<Vector2> ground = new List<Vector2>();
-        Vector2 startPos = new Vector2(-20, screenSize.y / 4);
+        Vector2 startPos = new Vector2(0, screenSize.y - 100);
         ground.Add(startPos);
-        float lenght = 0;
-        totalLenght = screenSize.x * 3;
+        float lenght = startPos.x;
+        // totalLenght = screenSize.x * 3;
         bool x = true;
         Vector2 newPoint = new Vector2(startPos);
         while(lenght < totalLenght){
+            GD.Print("Lenght: " + lenght);
             if(x){
                 lenght += rnd.RandfRange(minLengthX, maxLengthX);
                 newPoint.x = lenght;
             }
             else{
-                int up = rnd.RandiRange(0, 1);
-                up = up == 0 ? 1 : -1;
+                int dir = rnd.RandiRange(0, 1);
+                bool up = dir == 0 ? true : false;
                 float currentY = ground[ground.Count - 1].y;
                 
-                if(currentY + (maxLengthY * up) < (screenSize.y - maxLengthY)){
-                    newPoint.y = rnd.RandfRange(currentY + minLengthY, currentY + maxLengthY);
+                if(up){
+                    newPoint.y = CreateNewPoint(currentY, up, minLengthY, maxLengthY);
+                    if(newPoint.y > (screenSize.y - maxLengthY)){
+                        newPoint.y = CreateNewPoint(currentY, !up, minLengthY, maxLengthY);
+                    }
                 }
                 else{
-                    newPoint.y = rnd.RandfRange(currentY - minLengthY, currentY - maxLengthY);
+                    newPoint.y = CreateNewPoint(currentY, !up, minLengthY, maxLengthY);
+                    if(newPoint.y < (0 + maxLengthY)){
+                        newPoint.y = CreateNewPoint(currentY, !up, minLengthY, maxLengthY);
+                    }
                 }
             }
             x = !x;
-            var sas = ground[100000];
             ground.Add(newPoint);
         }
 
@@ -61,14 +75,23 @@ public class Ground : Node2D
         }
     }
 
-    public override void _Ready()
-    {
+    // public override void _Ready()
+    // {
         
-    }
+    // }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
 //  {
 //      
 //  }
+
+    public float CreateNewPoint(float current, bool up, float min, float max){
+        if(up){
+            return(rnd.RandfRange(current + min, current + max));
+        }
+        else{
+            return(rnd.RandfRange(current - max, current - min));
+        }
+    }
 }
